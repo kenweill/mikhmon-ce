6e36badaab07d0b9bc6bbd398988fa776a73a82d
kenweill <kenweilllumacad@gmail.com>
2026-05-08 10:36:56 +0800

    Bump version to CE 1.1

--- include/version.php
+++ include/version.php
@@ -2,5 +2,5 @@
 if (!isset($_SESSION["mikhmon"])) {
     header("Location:../admin.php?id=login");
   } else {
-        $_SESSION["v"] = "CE 1.0 (Based on MikhMon 3.20)";
+        $_SESSION["v"] = "CE 1.1 (Based on MikhMon 3.20)";
     }
