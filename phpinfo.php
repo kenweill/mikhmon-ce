9df94c9345a0cb59af27476142f44be9ed91c7e3
kenweill <kenweilllumacad@gmail.com>
2026-05-08 11:36:07 +0800

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,11 +1,3 @@
 <?php
-echo 'PHP version: ' . phpversion();
-?>
-<br />
-<hr />
-<?php 
-foreach (get_loaded_extensions() as $i => $ext)
-{
-   echo $ext .' => '. phpversion($ext). '<br/>';
-}
+phpinfo();
 ?>
